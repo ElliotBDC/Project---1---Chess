5037f577b1dc49e68494e63b381a5845e789c92a
ElliotBDC <35963237+ElliotBDC@users.noreply.github.com>
2023-07-28 18:14:19 +0100

    Created link b/w python and java programs
    - Servers working. Hard error to find where the servers messages to the client werent being received/printed by the client. This was fixed when a \n was inserted at the end of the message. Will have to research this.
    - Will change in the future when I decide the communication rules etc (maybe implementing UCI?)

--- server.py
+++ server.py
@@ -1,19 +1,49 @@
 import socket
+import threading
 
-def startServer():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
-        server.bind(('localhost', 12345))
-        server.listen(1)
-        client, address = server.accept()
-        with client:
-            print(f"Connected by {address}")
+client_sockets = []
+
+try:
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('localhost',19855))
+    server_socket.listen(2)
+
+    print("Server is ready to accept connections...")
+
+    def handle_client_connection(client_socket, client_address):
+        print(f"New connection from {client_address}")
+        try:
             while True:
-                data = client.recv(1024)
-                if not data:
+                message = client_socket.recv(1024)
+                client_socket.send(b"++\n")
+                if not message:
                     break
-                message = data
-                client.send(b"Recieved: " + message)
+                print(f"Received message from {client_address}")
+                print(f"Received: {str(message.decode('utf-8'))}")
+                for client in client_sockets:
+                    if client[0] != client_socket:
+                        client[0].send(f"{str(message.decode('utf-8'))}".encode('utf-8'))
 
-startServer()
+            print(f"Connection from {client_address} closed.")
+            client_socket.close()
+            client_sockets.pop(client_sockets.index((client_socket, client_address)))
+        except Exception as e:
+            print(f"An error occured: {e}")
+        finally:
+            client_socket.close()
+
+    while True:
+        client_socket, client_address = server_socket.accept()
+        client_sockets.append((client_socket, client_address))
+        client_thread = threading.Thread(target=handle_client_connection, args=(client_socket, client_address))
+        client_thread.start()
+
+except Exception as e:
+    print(f"An error occured: {e}")
+
+finally:
+    for client_socket, _ in client_sockets:
+        client_socket.close()
+    server_socket.close()
 
             
