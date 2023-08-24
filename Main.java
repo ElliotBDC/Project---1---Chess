c916bb956be21091b07ece335b512ee9dbf1ed6e
ElliotBDC <35963237+ElliotBDC@users.noreply.github.com>
2023-08-24 12:57:04 +0200

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        //Client.connectToServer("localhost", 12345);
-        Board.loadBoard();
-        String moves = Board.getAllPossibleMoves();
-        System.out.println(moves);
-    }
-}
